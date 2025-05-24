b0e1c881a1fc2f2f83a613e2c9b0e8b6229c5992
Mohammad Reza Faramarzi <mf575583@gmail.com>
2025-05-24 10:16:05 +0330

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="w-full flex items-center justify-between bg-blue-600 p-4">
             <div className="text-xl md:text-3xl text-blue-50 font-bold">
-                My Tontest Wallet
+                My TON Testnet wallet
             </div>
             <div>
                 <SimpleIconsTon className="w-10 h-10 md:w-12 md:h-12 text-blue-100" />
